781b968ab1ddfaa7d1d7de5ea5aab32dc5ecaae3
哲 陈 <376811578@qq.com>
2019-10-08 20:30:41 +0800

    test: nothing

--- .eslintrc.js
+++ .eslintrc.js
@@ -120,7 +120,7 @@ module.exports = {
         "block-scoped-var": 0,//块语句中使用var
         "brace-style": [1, "1tbs"],//大括号风格
         "callback-return": 1,//避免多次调用回调什么的
-        "camelcase": 2,//强制驼峰法命名
+        "camelcase": 0,//强制驼峰法命名
         "comma-dangle": [2, "never"],//对象字面量项尾不能有逗号
         "comma-spacing": 0,//逗号前后的空格
         "comma-style": [2, "last"],//逗号风格，换行时在行首还是行尾
